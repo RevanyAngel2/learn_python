a941ca17ab9c2cadf27d0c4fa44c249bafa951de
RevanyAngel2 <angelunilife@gmail.com>
2025-04-15 08:06:30 +0700

    innitial commit

---
+++ first.py
@@ -0,0 +1,28 @@
+from numpy import angle
+from sympy import true
+
+
+# print("hello world")
+# print("i'm learning python now, yeay!")
+# print("it's 15/4/2025")
+
+# nama = "Angel"
+# umur = 19
+# jurusan = "teknik informatika"
+# tinggi = 162.5
+# single = true
+
+# print("haloo, namaku", nama)
+# print("aku umurnya", umur, "tahun")
+# print("dari jurusan", jurusan)
+# print("tinggiku standar lah ya", tinggi)
+# print("apa aku single?", single)
+# print("high-class jomblo fii sabilillah ni senggol dong")
+
+a = 15
+b = 2
+print("aritmatika nya nih")
+print("hasil tambah:", a+b)
+print("hasil kurang:", a-b)
+print("hasil kali:", a*b)
+print("sisa bagi:", a%b)
